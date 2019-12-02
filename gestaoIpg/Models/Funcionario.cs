c0dffdb6b68ca45e8978b77a51bb6595bdfc936c
Henrique Moreira <hicasbatatas@hotmail.com>
2019-12-02 10:43:32 +0000

    Criação do modelo dos funcionários

---
+++ gestaoIpg/Models/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gestaoIpg.Models
+{
+    public class Funcionario
+    {
+        
+        public int FuncionarioId { get; set;}
+
+        [Required]
+        [StringLength(256)]
+        public String Nome { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public String Morada { get; set; }
+
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        public int Telemovel { get; set; }
+    }
+}
