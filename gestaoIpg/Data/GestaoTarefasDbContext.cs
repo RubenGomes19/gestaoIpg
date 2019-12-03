f629eabe9acc7bef3555229a15cfde9fccb4ee45
Ruben <RubenGomes19@users.noreply.github.com>
2019-12-03 18:57:53 +0000

    Criação BD gestao tarefas

--- gestaoIpg/Data/GestaoTarefasDbContext.cs
+++ gestaoIpg/Data/GestaoTarefasDbContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace gestaoIpg.Data
 {
-    public class GestaoTarefasDbContext
+    public class GestaoTarefasDbContext : DbContext
     {
+        public GestaoTarefasDbContext(DbContextOptions<GestaoTarefasDbContext> options)
+            : base(options)
+        {
+        }
     }
 }
