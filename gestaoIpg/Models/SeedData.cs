db20d42b1f90b0710463b2b67c817aad05d3ca1f
Ruben <RubenGomes19@users.noreply.github.com>
2019-12-11 16:51:32 +0000

    SeedData

---
+++ gestaoIpg/Models/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gestaoIpg.Models
+{
+    public class SeedData
+    {
+        public static void Populate(gestaoIpgDbContext db)
+        {
+            PopulateDepartamento(db);
+           
+        }
+
+        private static void PopulateDepartamento(gestaoIpgDbContext db)
+        {
+            if (db.Departamento.Any()) return;
+            db.Departamento.AddRange(
+                new Departamento { Tipo = "Departamento informática" },
+                new Departamento { Tipo = "Departamento contabilidade" },
+                new Departamento { Tipo = "Departamento gestao" }
+            );
+            db.SaveChanges();
+        }
+    }
+}
