91382b8af071ce834ca87b4253eddbc7068338df
Cheng Bo <1700637@sal.ipg.pt>
2019-12-19 00:51:59 +0000

    Novo migration

--- gestaoIpg/Migrations/20191219005017_last.Designer.cs
+++ gestaoIpg/Migrations/20191219005017_last.Designer.cs
@@ -2,14 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace gestaoIpg.Migrations
 {
     [DbContext(typeof(gestaoIpgDbContext))]
-    partial class gestaoIpgDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191219005017_last")]
+    partial class last
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
